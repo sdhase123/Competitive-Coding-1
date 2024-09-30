98d9aad8e1118eb17a200bc83dc2a94b26c604a6
sdhase123 <51932114+sdhase123@users.noreply.github.com>
2024-09-30 18:13:26 -0500

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,21 @@
+// Array is not sorted.
+// Use HashSet to find the missing number.
+// time complexity o(n)
+// Space complexity o(n) for storing nums in hashset;
 
+class Solution {
+    public int missingNumber(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for(int i : nums) {
+            numSet.add(i);
+        }
+
+        for(int i = 0;i<nums.length+1;i++) {
+            if(!numSet.contains(i)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
